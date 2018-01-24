6666309de3ec7f1e5ae1e9a142121dd24f9f2017
Tyler Ipson <tyler.ipson@clearlink.com>
2018-01-24 13:57:30 -0700

    In with the new

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,139 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Currency from './Currency';
+import fire, { timestamp } from '../fire';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { currencies: {} };
+  }
+  componentWillMount() {
+    // Rely on localStorage initially to prevent annoying 1000ms delay or so from Firebase..
+    if (localStorage.getItem('cachedCurrencies')) {
+      // Turn the stored data back into an object..
+      let cached = JSON.parse(localStorage.getItem('cachedCurrencies'));
+      // For some reason, null/empty is set as the first array item when storing it here...
+      cached = cached.slice(1);
+      this.setState({ currencies: cached });
+    }
+
+    // Get latest data from Firebase
+    // Set as both state, and update the localStorage
+    fire
+      .database()
+      .ref('currencies')
+      .on('value', snap => {
+        this.setState({ currencies: snap.val() });
+        localStorage.setItem('cachedCurrencies', JSON.stringify(snap.val()));
+      });
+  }
+  componentDidMount() {
+    // CMC API updates only once every 5 minutes
+    // Store some date timestamps so we can know if the data is older than that or not..
+    const ref = fire.database().ref('sessions');
+
+    ref.update({
+      lastChecked: timestamp,
+    });
+
+    ref.once('value', snap => {
+      const val = snap.val();
+      if (val.lastChecked > val.lastUpdated + 300000) {
+        // Firebase data is older than 5 minutes.. let's go update it.
+        this.importData();
+      }
+    });
+  }
+  importData() {
+    fetch('https://api.coinmarketcap.com/v1/ticker/?')
+      .then(res => res.json())
+      .then(data => {
+        const ref = fire
+          .database()
+          .ref()
+          .child('currencies');
+
+        data.map(el => {
+          const obj = {};
+          obj[el.rank] = el;
+
+          console.log(obj);
+
+          ref.update(obj);
+        });
+      })
+      .then(() => {
+        fire
+          .database()
+          .ref('sessions')
+          .update({
+            lastUpdated: timestamp,
+          });
+      });
+  }
+  buildCurrencies() {
+    const items = [];
+    for (const obj in this.state.currencies) {
+      items.push(<Currency key={this.state.currencies[obj].id} data={this.state.currencies[obj]} />);
+    }
+    return items;
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App__header">
-          <h1 className="App__title">Cryptab</h1>
-        </header>
-        <p className="App__intro">Cryptocurrencies Ranked by Market Cap</p>
+      <div className="content">
+        <div className="header">
+          <div className="left">
+            <a href="http://cryptab.io">
+              <img className="logo light" src="icon_white_256.png" alt="Cryptab" />
+              <img className="logo dark" src="icon_dark_256.png" alt="Cryptab" />
+              <h1>Cryptab</h1>
+            </a>
+          </div>
+          <div className="middle">
+            <h1>
+              <span className="title">Cryptocurrencies Ranked by Market Cap</span>
+            </h1>
+          </div>
+          <div className="right">
+            <input type="checkbox" className="switch" id="switch-1" />
+          </div>
+        </div>
+        <div className="container">{this.buildCurrencies()}</div>
+
+        <div className="ad">
+          <div className="left">
+            <img src="images/qr.png" width="100" alt="QR Code" />
+          </div>
+          <div className="right">
+            <h3>Enjoying Cryptab?</h3>
+            <p>Bitcoin: 1nzhgrYVnnD1fHP1WVZVBB2qxvtV4Actt</p>
+            <p>Ethereum: 0x85BfB8CDba1B5b05138932BF975E4503B47Be9c1</p>
+            <br />
+            <p>
+              <small>
+                <a href="https://www.coinbase.com/join/549320a9cfb322ab750000fb" className="underline">
+                  New to Crypto? Sign up with Coinbase and receive $10 in Bitcoin when you make your first deposit!
+                </a>{' '}
+                (ad)
+              </small>
+            </p>
+          </div>
+        </div>
+
+        <div className="show-more">
+          <a className="button" data-list="limit=500">
+            Show Top 500
+          </a>
+        </div>
+
+        <div className="footer">
+          <p>
+            Cryptab &copy; 2017 | Design by <a href="http://www.evachau.com/">Eva Chau</a> | Powered by{' '}
+            <a href="https://coinmarketcap.com/">CoinMarketCap</a>
+          </p>
+        </div>
       </div>
     );
   }
--- src/components/Currency.js
+++ src/components/Currency.js
@@ -1,7 +1,77 @@
 import React, { Component } from 'react';
+import './Currency.css';
 
 export default class Currency extends Component {
+  constructor(props) {
+    super(props);
+    this.state = this.props.data;
+  }
   render() {
-    <div className="currency">I am currency, yeah</div>;
+    const numFormat = num => {
+      const thousand = 1000;
+      const million = 1000000;
+      const billion = 1000000000;
+
+      if (num / billion > 1) {
+        return (num / billion).toFixed(2) + 'B';
+      } else if (num / million > 1) {
+        return (num / million).toFixed(2) + 'M';
+      } else if (num / thousand > 100) {
+        return (num / thousand).toFixed(2) + 'K';
+      } else {
+        return (num / 1).toFixed(2);
+      }
+    };
+
+    const roundPrice = (price = this.state.price_usd) => {
+      if (price < 1) {
+        return (price / 1).toFixed(6);
+      }
+      return (price / 1).toFixed(2);
+    };
+    return (
+      <div className={`currency ${this.state.percent_change_24h > 0 ? 'up' : 'down'}`}>
+        <div className="rank">
+          <h2>{this.state.rank}</h2>
+        </div>
+        <div className="logo">
+          <img src={`https://files.coinmarketcap.com/static/img/coins/128x128/${this.state.id}.png`} />
+        </div>
+        <div className="name">
+          <h2 className="full">
+            <a href={`https://coinmarketcap.com/currencies/${this.state.id}/`} target="_blank">
+              {this.state.name}
+            </a>
+          </h2>
+          <span className="short">{this.state.symbol}</span>
+        </div>
+        <div className="price">
+          <a href={`https://coinmarketcap.com/currencies/${this.state.id}/#markets`} target="_blank">
+            <h2>${roundPrice()}</h2>
+          </a>
+        </div>
+        <div className="change">
+          <span className="label">Change (24h)</span>
+          <a href={`https://coinmarketcap.com/currencies/${this.state.id}/#charts`} target="_blank">
+            <h2>
+              <img id="trend" src="//:0" />
+              {this.state.percent_change_24h}%
+            </h2>
+          </a>
+        </div>
+        <div className="market">
+          <span className="label">Market Cap</span>
+          <a href={`https://coinmarketcap.com/currencies/${this.state.id}/`} target="_blank">
+            <h2>${numFormat(this.state.market_cap_usd)}</h2>
+          </a>
+        </div>
+        <div className="volume">
+          <span className="label">Volume (24h)</span>
+          <a href={`https://coinmarketcap.com/currencies/${this.state.id}/#markets`} target="_blank">
+            <h2>${numFormat(this.state['24h_volume_usd'])}</h2>
+          </a>
+        </div>
+      </div>
+    );
   }
 }
