324a9ae5871687b27dea8408bd74a0b0698e511f
Tyler Ipson <tyler.ipson@clearlink.com>
2018-01-26 10:28:47 -0700

    Refactor to use coincap and Redux

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -1,11 +1,7 @@
 import React, { Component } from 'react';
 import './Currency.css';
 
-export default class Currency extends Component {
-  constructor(props) {
-    super(props);
-    this.state = this.props.data;
-  }
+class Currency extends Component {
   render() {
     const numFormat = num => {
       const thousand = 1000;
@@ -23,55 +19,58 @@ export default class Currency extends Component {
       }
     };
 
-    const roundPrice = (price = this.state.price_usd) => {
+    const roundPrice = price => {
       if (price < 1) {
-        return (price / 1).toFixed(6);
+        return (price / 1).toFixed(5);
       }
-      return (price / 1).toFixed(2);
+      return (price / 1).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',');
     };
+    const { cap24hrChange, mktcap, price, long, usdVolume, short } = this.props.currencies[this.props.short];
+
+    // const rank = this.props.index + 1;
     return (
-      <div className={`currency ${this.state.percent_change_24h > 0 ? 'up' : 'down'}`}>
-        <div className="rank">
-          <h2>{this.state.rank}</h2>
-        </div>
+      <div className={`currency ${cap24hrChange > 0 ? 'up' : 'down'}`}>
+        <div className="rank">{<h2>{this.props.rank}</h2>}</div>
         <div className="logo">
-          <img src={`https://files.coinmarketcap.com/static/img/coins/128x128/${this.state.id}.png`} />
+          {/* <img src={`https://files.coinmarketcap.com/static/img/coins/128x128/${short}.png`} /> */}
         </div>
         <div className="name">
           <h2 className="full">
-            <a href={`https://coinmarketcap.com/currencies/${this.state.id}/`} target="_blank">
-              {this.state.name}
+            <a href={`https://coinmarketcap.com/currencies/${short}/`} target="_blank">
+              {long}
             </a>
           </h2>
-          <span className="short">{this.state.symbol}</span>
+          <span className="short">{short}</span>
         </div>
         <div className="price">
-          <a href={`https://coinmarketcap.com/currencies/${this.state.id}/#markets`} target="_blank">
-            <h2>${roundPrice()}</h2>
+          <a href={`https://coinmarketcap.com/currencies/${short}/#markets`} target="_blank">
+            <h2>${roundPrice(price)}</h2>
           </a>
         </div>
         <div className="change">
           <span className="label">Change (24h)</span>
-          <a href={`https://coinmarketcap.com/currencies/${this.state.id}/#charts`} target="_blank">
+          <a href={`https://coinmarketcap.com/currencies/${short}/#charts`} target="_blank">
             <h2>
               <img id="trend" src="//:0" />
-              {this.state.percent_change_24h}%
+              {cap24hrChange}%
             </h2>
           </a>
         </div>
         <div className="market">
           <span className="label">Market Cap</span>
-          <a href={`https://coinmarketcap.com/currencies/${this.state.id}/`} target="_blank">
-            <h2>${numFormat(this.state.market_cap_usd)}</h2>
+          <a href={`https://coinmarketcap.com/currencies/${short}/`} target="_blank">
+            <h2>${numFormat(mktcap)}</h2>
           </a>
         </div>
         <div className="volume">
           <span className="label">Volume (24h)</span>
-          <a href={`https://coinmarketcap.com/currencies/${this.state.id}/#markets`} target="_blank">
-            <h2>${numFormat(this.state['24h_volume_usd'])}</h2>
+          <a href={`https://coinmarketcap.com/currencies/${short}/#markets`} target="_blank">
+            <h2>${numFormat(usdVolume)}</h2>
           </a>
         </div>
       </div>
     );
   }
 }
+
+export default Currency;
