ad955022f5e698d7b46a60c65f69a303fe31e54e
Tyler Ipson <tyler.ipson@clearlink.com>
2018-01-27 17:38:18 -0700

    Set up an app container that we can add the theme class to

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './Main.css';
 import Currency from './Currency';
+import ThemeSwitcher from './ThemeSwitcher';
 import socketClient from 'socket.io-client';
 
 class Main extends Component {
@@ -19,63 +20,65 @@ class Main extends Component {
     this.props.currencies.map((currency, i) => <Currency />);
   }
   render() {
-    return (
-      <div className="content">
-        <div className="header">
-          <div className="left">
-            <a href="http://cryptab.io">
-              <img className="logo light" src="icon_white_256.png" alt="Cryptab" />
-              <img className="logo dark" src="icon_dark_256.png" alt="Cryptab" />
-              <h1>Cryptab</h1>
-            </a>
-          </div>
-          <div className="middle">
-            <h1>
-              <span className="title">Cryptocurrencies Ranked by Market Cap</span>
-            </h1>
-          </div>
-          <div className="right">
-            <input type="checkbox" className="switch" id="switch-1" />
-          </div>
-        </div>
-        <div className="container">
-          {Object.keys(this.props.currencies).map((currency, i) => {
-            const { short } = this.props.currencies[currency];
-            return <Currency {...this.props} key={short} short={short} rank={(i = i + 1)} />;
-          })}
-        </div>
+    const appClasses = ['app-container'];
 
-        <div className="ad">
-          <div className="left">
-            <img src="images/qr.png" width="100" alt="QR Code" />
+    if (this.props.lightTheme.enabled) {
+      appClasses.push('light');
+    }
+
+    return (
+      <div className={appClasses.join(' ')}>
+        <div className="content">
+          <div className="header">
+            <div className="left">
+              <a href="http://cryptab.io">
+                <img className="logo light" src="icon_white_256.png" alt="Cryptab" />
+                <img className="logo dark" src="icon_dark_256.png" alt="Cryptab" />
+                <h1>Cryptab</h1>
+              </a>
+            </div>
+            <div className="middle">
+              <h1>
+                <span className="title">Cryptocurrencies Ranked by Market Cap</span>
+              </h1>
+            </div>
+            <div className="right">
+              <ThemeSwitcher {...this.props} />
+            </div>
           </div>
-          <div className="right">
-            <h3>Enjoying Cryptab?</h3>
-            <p>Bitcoin: 1nzhgrYVnnD1fHP1WVZVBB2qxvtV4Actt</p>
-            <p>Ethereum: 0x85BfB8CDba1B5b05138932BF975E4503B47Be9c1</p>
-            <br />
+          <div className="container">
+            {Object.keys(this.props.currencies).map((currency, i) => {
+              const { short } = this.props.currencies[currency];
+              return <Currency {...this.props} key={short} short={short} rank={(i = i + 1)} />;
+            })}
+          </div>
+
+          <div className="ad">
+            <div className="left">
+              <img src="images/qr.png" width="100" alt="QR Code" />
+            </div>
+            <div className="right">
+              <h3>Enjoying Cryptab?</h3>
+              <p>Bitcoin: 1nzhgrYVnnD1fHP1WVZVBB2qxvtV4Actt</p>
+              <p>Ethereum: 0x85BfB8CDba1B5b05138932BF975E4503B47Be9c1</p>
+              <br />
+              <p>
+                <small>
+                  <a href="https://www.coinbase.com/join/549320a9cfb322ab750000fb" className="underline">
+                    New to Crypto? Sign up with Coinbase and receive $10 in Bitcoin when you make your first deposit!
+                  </a>{' '}
+                  (ad)
+                </small>
+              </p>
+            </div>
+          </div>
+
+          <div className="footer">
             <p>
-              <small>
-                <a href="https://www.coinbase.com/join/549320a9cfb322ab750000fb" className="underline">
-                  New to Crypto? Sign up with Coinbase and receive $10 in Bitcoin when you make your first deposit!
-                </a>{' '}
-                (ad)
-              </small>
+              Cryptab &copy; 2017 | Design by <a href="http://www.evachau.com/">Eva Chau</a> | Powered by{' '}
+              <a href="https://coincap.com/">coincap</a>
             </p>
           </div>
-        </div>
-
-        <div className="show-more">
-          <a className="button" data-list="limit=500">
-            Show Top 500
-          </a>
-        </div>
-
-        <div className="footer">
-          <p>
-            Cryptab &copy; 2017 | Design by <a href="http://www.evachau.com/">Eva Chau</a> | Powered by{' '}
-            <a href="https://coinmarketcap.com/">CoinMarketCap</a>
-          </p>
         </div>
       </div>
     );
